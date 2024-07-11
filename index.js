abff3a8e7802786d154934da4a140a9cafc02882
plug_keith <keith.mwai@student.moringaschool.com>
2024-07-11 22:53:57 +0300

    Js-submit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+      const paragraph = document.getElementById("text");
+      paragraph.textContent = "This is really cool!";
+    });
